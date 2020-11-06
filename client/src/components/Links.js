266b22cab90313141a9f2a8a835880cd59b1c021
emre <emre>
2020-11-07 00:59:15 +0200

    components

---
+++ client/src/components/Links.js
@@ -0,0 +1,17 @@
+import React, { Fragment } from "react"
+import "./App.css"
+
+import Header from "./components/Header"
+import List from "./components/List"
+
+function Links() {
+	return (
+		<div class="tm-container">
+			<div class="tm-row">
+				<Header />
+			</div>
+		</div>
+	)
+}
+
+export default Links
