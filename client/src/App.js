7d02bf5ee6a016e90b20aef4acf410baf2b35761
emre <emre>
2020-11-07 00:58:04 +0200

    footer

--- client/src/App.js
+++ client/src/App.js
@@ -2,13 +2,16 @@ import React, { Fragment } from "react"
 import "./App.css"
 
 import Header from "./components/Header"
+import List from "./components/List"
 
 function App() {
 	return (
 		<div class="tm-container">
-    <div class="tm-row">
-        </div>
-      </div>
+			<div class="tm-row">
+				<Header />
+			</div>
+		</div>
+	)
 }
 
 export default App
